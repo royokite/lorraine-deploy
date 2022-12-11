7bd2423085e5ebcb16a85218286ee14909752fb6
RoyOkite <royokite@gmail.com>
2022-12-11 09:49:45 +0300

    Routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/hello', to: 'application#hello_world'
+  resources :students, only: [:index, :create, :show, :update, :destroy] do
+    resources :transactions, only: [:index, :create, :show]    
+  end
+  
+  resources :instructors, only: [:index, :create, :show, :update, :destroy]
+  resources :schedules, only: [:index, :create, :show, :update, :destroy]
+  resources :courses, only: [:index, :create, :show, :update, :destroy]
+  resources :subjects, only: [:index, :create, :show, :update, :destroy]
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
 end
